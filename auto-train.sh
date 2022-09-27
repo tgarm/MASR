91fa999d4fe47d08b117e028bd0022a4066d84e0
dxl <dxl@britao.com>
2022-09-27 08:53:42 +0800

    append log

--- auto-train.sh
+++ auto-train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python train.py --batch_size=8 --pinyin_mode=True --feature_method=fbank |tee auto-train.log
+python train.py --batch_size=8 --pinyin_mode=True --feature_method=fbank |tee -a auto-train.log
 
